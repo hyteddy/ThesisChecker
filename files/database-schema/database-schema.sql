ccaeb532380f8691ff920c8f1c2bd23d947a6f88
awilczyn <and.wilczynski@gmail.com>
2014-03-30 16:29:51 +0200

    #1Create database schema-update

--- files/database-schema/database-schema.sql
+++ files/database-schema/database-schema.sql
@@ -1,6 +1,6 @@
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
 USE `mydb` ;
@@ -8,23 +8,18 @@ USE `mydb` ;
 -- -----------------------------------------------------
 -- Table `mydb`.`user`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`user` (
-  `id` INT NULL DEFAULT NULL AUTO_INCREMENT ,
-  `first_name` VARCHAR(255) NULL DEFAULT NULL ,
-  `last_name` VARCHAR(255) NULL DEFAULT NULL ,
-  `email` VARCHAR(255) NULL DEFAULT NULL ,
-  `address` VARCHAR(255) NULL DEFAULT NULL ,
-  `postal_code` VARCHAR(128) NULL DEFAULT NULL ,
-  `phone` VARCHAR(255) NULL DEFAULT NULL ,
-  `salt` VARCHAR(255) NULL DEFAULT NULL ,
-  `password` VARCHAR(255) NULL DEFAULT NULL ,
-  `role` VARCHAR(255) NULL DEFAULT NULL ,
-  `token` VARCHAR(255) NULL DEFAULT NULL ,
-  `active` TINYINT(1) NULL DEFAULT '1' ,
-  `created_at` DATETIME NOT NULL ,
-  `updated_at` DATETIME NOT NULL ,
-  `deleted_at` DATETIME NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `id` INT NULL DEFAULT NULL AUTO_INCREMENT,
+  `email` VARCHAR(255) NULL DEFAULT NULL,
+  `salt` VARCHAR(255) NULL DEFAULT NULL,
+  `password` VARCHAR(255) NULL DEFAULT NULL,
+  `role` VARCHAR(255) NULL DEFAULT NULL,
+  `token` VARCHAR(255) NULL DEFAULT NULL,
+  `active` TINYINT(1) NULL DEFAULT '1',
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8;
 
@@ -32,24 +27,19 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `mydb`.`analysis`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`analysis` (
-  `id` INT NULL DEFAULT NULL AUTO_INCREMENT ,
-  `date` DATETIME NULL DEFAULT NULL ,
-  `description` LONGTEXT NULL DEFAULT NULL ,
-  `result` DECIMAL(18,2) NULL DEFAULT NULL ,
-  `file_type` VARCHAR(255) NULL DEFAULT NULL ,
-  `file` LONGBLOB NULL DEFAULT NULL ,
-  `user_id` INT NULL DEFAULT NULL ,
-  `created_at` DATETIME NOT NULL ,
-  `updated_at` DATETIME NOT NULL ,
-  `deleted_at` DATETIME NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `id` (`user_id` ASC) ,
-  CONSTRAINT `id`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`user` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`analysis` (
+  `id` INT NULL DEFAULT NULL AUTO_INCREMENT,
+  `date` DATETIME NULL DEFAULT NULL,
+  `description` LONGTEXT NULL DEFAULT NULL,
+  `result` DECIMAL(18,2) NULL DEFAULT NULL,
+  `file_type` VARCHAR(255) NULL DEFAULT NULL,
+  `file` LONGBLOB NULL DEFAULT NULL,
+  `user_id` INT NULL DEFAULT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`user_id` ASC))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8;
 
@@ -57,23 +47,18 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `mydb`.`resource`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`resource` (
-  `id` INT NULL DEFAULT NULL AUTO_INCREMENT ,
-  `name` VARCHAR(255) NULL DEFAULT NULL ,
-  `description` LONGTEXT NULL DEFAULT NULL ,
-  `file_type` VARCHAR(255) NULL DEFAULT NULL ,
-  `file` LONGBLOB NULL DEFAULT NULL ,
-  `user_id` INT NULL DEFAULT NULL ,
-  `created_at` DATETIME NOT NULL ,
-  `updated_at` DATETIME NOT NULL ,
-  `deleted_at` DATETIME NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `id` (`user_id` ASC) ,
-  CONSTRAINT `id`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`user` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`resource` (
+  `id` INT NULL DEFAULT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `description` LONGTEXT NULL DEFAULT NULL,
+  `file_type` VARCHAR(255) NULL DEFAULT NULL,
+  `file` LONGBLOB NULL DEFAULT NULL,
+  `user_id` INT NULL DEFAULT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`user_id` ASC))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8;
 
@@ -81,18 +66,13 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `mydb`.`user_update`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`user_update` (
-  `id` INT NULL DEFAULT NULL AUTO_INCREMENT ,
-  `user_id` INT NULL DEFAULT NULL ,
-  `token` VARCHAR(255) NULL DEFAULT NULL ,
-  `type` VARCHAR(255) NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) ,
-  INDEX `id` (`user_id` ASC) ,
-  CONSTRAINT `id`
-    FOREIGN KEY (`user_id` )
-    REFERENCES `mydb`.`user` (`id` )
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`user_update` (
+  `id` INT NULL DEFAULT NULL AUTO_INCREMENT,
+  `user_id` INT NULL DEFAULT NULL,
+  `token` VARCHAR(255) NULL DEFAULT NULL,
+  `type` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`user_id` ASC))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8;
 
@@ -100,25 +80,38 @@ DEFAULT CHARACTER SET = utf8;
 -- -----------------------------------------------------
 -- Table `mydb`.`analysis_has_resource`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `mydb`.`analysis_has_resource` (
-  `analysis_id` INT NOT NULL ,
-  `resource_id` INT NOT NULL ,
-  PRIMARY KEY (`analysis_id`, `resource_id`) ,
-  INDEX `fk_analysis_has_resource_resource1` (`resource_id` ASC) ,
-  INDEX `fk_analysis_has_resource_analysis1` (`analysis_id` ASC) ,
-  CONSTRAINT `fk_analysis_has_resource_analysis1`
-    FOREIGN KEY (`analysis_id` )
-    REFERENCES `mydb`.`analysis` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_analysis_has_resource_resource1`
-    FOREIGN KEY (`resource_id` )
-    REFERENCES `mydb`.`resource` (`id` )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+CREATE TABLE IF NOT EXISTS `mydb`.`analysis_has_resource` (
+  `analysis_id` INT NOT NULL,
+  `resource_id` INT NOT NULL,
+  PRIMARY KEY (`analysis_id`, `resource_id`),
+  INDEX `fk_analysis_has_resource_resource1_idx` (`resource_id` ASC),
+  INDEX `fk_analysis_has_resource_analysis1_idx` (`analysis_id` ASC))
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8;
 
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user_profile`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`user_profile` (
+  `id` INT NULL AUTO_INCREMENT,
+  `user_id` INT NULL DEFAULT NULL,
+  `first_name` VARCHAR(255) NULL DEFAULT NULL,
+  `last_name` VARCHAR(255) NULL DEFAULT NULL,
+  `address` VARCHAR(255) NULL DEFAULT NULL,
+  `postal_code` VARCHAR(255) NULL DEFAULT NULL,
+  `phone` VARCHAR(255) NULL DEFAULT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NOT NULL,
+  `deleted_at` DATETIME NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `id_idx` (`user_id` ASC),
+  CONSTRAINT `id`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `mydb`.`user` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
