aa72fac65d0f65fa7d4ddde3c338d4aeaf487410
tomek__1990 <t.morek@gmail.com>
2014-03-31 22:12:01 +0200

    #27 Create Maven application project.

---
+++ WebApplication/app-java/src/main/java/com/thesischecker/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.thesischecker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Test controller
+ * @author Tomasz Morek
+ */
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+    /**
+     * Index text method
+     * @return view name
+     */
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+}
---
+++ WebApplication/app-web/src/main/webapp/WEB-INF/jsp/index.jsp
@@ -0,0 +1,1 @@
+<h1>ThesisChecker test view</h1>
