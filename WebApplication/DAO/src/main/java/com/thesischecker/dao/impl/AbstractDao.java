0547075698700d669e565811799593eadbf39ab0
m4gik <m4giksoftware@gmail.com>
2014-06-04 10:00:34 +0200

    Save changes before merge

--- WebApplication/DAO/src/main/java/com/thesischecker/dao/impl/AbstractDao.java
+++ WebApplication/DAO/src/main/java/com/thesischecker/dao/impl/AbstractDao.java
@@ -1,17 +1,19 @@
 package com.thesischecker.dao.impl;
 
-import com.thesischecker.dao.interfaces.IAbstractDao;
-import com.thesischecker.dto.HibernateUtil;
+import java.io.Serializable;
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 
-import java.io.Serializable;
-import java.util.List;
+import com.thesischecker.dao.interfaces.IAbstractDao;
+import com.thesischecker.dto.HibernateUtil;
 
 /**
  * Implementation AbstractDao
+ * 
  * @author Tomasz Morek
  */
 public abstract class AbstractDao<E, I extends Serializable> implements
@@ -29,6 +31,7 @@ public abstract class AbstractDao<E, I extends Serializable> implements
 
     /**
      * Constructor
+     * 
      * @param dtoClass
      */
     protected AbstractDao(Class<E> dtoClass) {
@@ -36,8 +39,26 @@ public abstract class AbstractDao<E, I extends Serializable> implements
         this.sessionFactory = HibernateUtil.getSessionFactory();
     }
 
+    @Override
+    public void delete(Object o) {
+        this.getSession().delete(o);
+    }
+
+    @Override
+    public List find(Criterion criterion) {
+        Criteria criteria = this.getSession().createCriteria(this.dtoClass);
+        criteria.add(criterion);
+        return criteria.list();
+    }
+
+    @Override
+    public E findById(Long id) {
+        return (E) this.getSession().get(this.dtoClass, id);
+    }
+
     /**
      * Get current session from SessionFactory
+     * 
      * @return Session
      */
     protected Session getSession() {
@@ -47,22 +68,5 @@ public abstract class AbstractDao<E, I extends Serializable> implements
     @Override
     public void saveOrUpdate(Object o) {
         this.getSession().saveOrUpdate(o);
-    }
-
-    @Override
-    public void delete(Object o) {
-        this.getSession().delete(o);
-    }
-
-    @Override
-    public E findById(Long id) {
-        return (E) this.getSession().get(this.dtoClass, id);
-    }
-
-    @Override
-    public List find(Criterion criterion) {
-        Criteria criteria = this.getSession().createCriteria(this.dtoClass);
-        criteria.add(criterion);
-        return criteria.list();
     }
 }
