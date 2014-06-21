d1826ff936a84ba89ec83870b925abb435ae5c6f
m4gik <m4giksoftware@gmail.com>
2014-06-21 14:10:36 +0200

    #67 Fix test failures

--- WebApplication/app-java/src/test/java/com/thesischecker/filesystem/FileProcessTest.java
+++ WebApplication/app-java/src/test/java/com/thesischecker/filesystem/FileProcessTest.java
@@ -2,57 +2,67 @@ package com.thesischecker.filesystem;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.net.URL;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 
 /**
  * Created by awilczyn on 27.04.2014.
  */
+@RunWith(value = BlockJUnit4ClassRunner.class)
 public class FileProcessTest extends TestCase {
 
     String currentDir = System.getProperty("user.dir");
 
-    @Test(expected = IOException.class)
-    public void checkConsistencyMechanismNegativeScenario() {
+    @Test(expected = FileNotFoundException.class)
+    public void testConsistencyMechanismNegativeScenario()
+            throws FileNotFoundException {
+        // What
         FileProcess fileProcess = new FileProcess(null);
+        // When
         fileProcess.consistencyMechanism();
+        // Then ... throw new Exception
     }
 
     @Test
-    public void checkConsistencyMechanismPositiveScenario() {
-        FileProcess fileProcess = new FileProcess(
-                currentDir
-                        + "/WebApplication/app-java/src/main/resources/files/test6295411515061869163.md");
+    public void testConsistencyMechanismPositiveScenario() throws Exception {
+        System.out.println(currentDir);
+        FileProcess fileProcess = new FileProcess(currentDir
+                + "/src/main/resources/files/test6295411515061869163.md");
         fileProcess.consistencyMechanism();
     }
 
     @Test
     public void testCreateTempFile() throws Exception {
         FileProcess fileP = new FileProcess(currentDir
-                + "/WebApplication/app-java/src/main/resources/files");
+                + "/src/main/resources/files");
         File uploadFile = new File(currentDir
-                + "/WebApplication/app-java/src/test/resources/testow.towy.md");
+                + "/src/test/resources/testow.towy.md");
         File test = fileP.createTempFile(uploadFile);
     }
 
     @Test
     public void testDeleteFileFromDisk() throws Exception {
-        FileProcess fileP = new FileProcess(
-                currentDir
-                        + "/WebApplication/app-java/src/main/resources/files/test584778542838682843.md");
+        FileProcess fileP = new FileProcess(currentDir
+                + "/src/main/resources/files");
+        File uploadFile = new File(currentDir
+                + "/src/test/resources/testow.towy.md");
+        File test = fileP.createTempFile(uploadFile);
+        FileProcess fileToDelete = new FileProcess(currentDir
+                + "/src/main/resources/files/" + test.getName());
 
         fileP.deleteFileFromDisk();
     }
 
     @Test
     public void testGetFileFromDisk() throws Exception {
-        FileProcess fileP = new FileProcess(
-                currentDir
-                        + "/WebApplication/app-java/src/main/resources/files/test584778542838682843.md");
+        FileProcess fileP = new FileProcess(currentDir
+                + "/src/main/resources/files/test6295411515061869163.md");
         FileInputStream test = fileP.getFileFromDisk();
     }
 
@@ -61,7 +71,7 @@ public class FileProcessTest extends TestCase {
         URL resourceUrl = getClass().getResource("/test.md");
         File uploadFile = new File(resourceUrl.getPath());
         FileProcess fileP = new FileProcess(currentDir
-                + "/WebApplication/app-java/src/main/resources/files");
+                + "/src/main/resources/files");
         fileP.saveFileOnDisk(uploadFile);
     }
 }
